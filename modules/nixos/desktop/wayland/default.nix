b7412dd07840eaad6d6d6f8e073ea9b7c4ab625f
Sayokeoda <sayokeoda@proton.me>
2026-04-17 20:54:59 +0200

    Añadido niri y configurado Wayland

--- modules/nixos/desktop/wayland/default.nix
+++ modules/nixos/desktop/wayland/default.nix
@@ -1,5 +1,28 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [ foot neovim fuzzel firefox ];
-#  services.mako.enable = true;
+  imports = [
+    ./niri.nix
+  ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+    config.common.default = "*";
+  };
+
+  fonts.packages = with pkgs; [
+    nerdfonts
+    noto-fonts
+  ];
+
+  environment.systemPackages = with pkgs; [
+    wayland-utils
+    wl-clipboard
+    xwayland
+  ];
 }
---
+++ modules/nixos/desktop/wayland/niri.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  programs.niri.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd niri";
+        user = "greeter"
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    foot
+    neovim
+  ];
+};
