845887fc522e404b78c85064fdee21cd0982bd4f
Sayokeoda <sayokeoda@proton.me>
2026-04-18 20:50:16 +0200

    Repathing neovim & foot in home manager

--- home/default.nix
+++ home/default.nix
@@ -10,11 +10,33 @@
 #    ./desktop/shell/quickshell
   ];
 
-  home.packages = with pkgs; [
-    neovim
-    foot
-    firefox
-  ];
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        term = "xterm-256color";
+        font = "JetBrainsMono Nerd Font:size=11";
+        dpi-aware = "yes";
+      };
+
+        color = {
+          alpha = "0.9";
+          background = "1a1b26";
+          foreground = "c0caf5";
+        };
+
+        mouse = {
+          hide-when-typing = "yes";
+        };
+      };
+    };
+
+  programs.neovim = {
+    enable = true;
+
+    withRuby = false;
+    withPython3 = false;
+  };
 
   programs.home-manager.enable = true;
 }
