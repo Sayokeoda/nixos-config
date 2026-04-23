85994bb8a29d9ed9faeffeaf98f63a0c654d7dcc
Sayokeoda <sayokeoda@proton.me>
2026-04-23 19:58:25 +0200

    Add user option for greetd configuration

--- modules/nixos/desktop/wayland/greetd.nix
+++ modules/nixos/desktop/wayland/greetd.nix
@@ -3,19 +3,27 @@
 {
   options.modules.greetd = {
     enable = lib.mkEnableOption "greetd display manager";
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "greeter";
+      description = "Usuario que ejecuta la sesión de greetd";
+    };
+
     sessionCommand = lib.mkOption {
       type = lib.types.str;
       default = "";
-      description = "Comando de sesión por defecto";
+      description = "ejecuta la sesión (niri)";
     };
   };
 
   config = lib.mkIf config.modules.greetd.enable {
     services.greetd = {
       enable = true;
+
       settings.default_session = {
         command = config.modules.greetd.sessionCommand;
-        user = "Sayokeoda"; # cámbialo
+        user = config.modules.greetd.user;
       };
     };
   };
