d95dd5e365389bf59a5c94dba6ccd176cc20d7cf
Sayokeoda <sayokeoda@proton.me>
2026-04-23 19:47:45 +0200

    Update greetd.nix

--- modules/nixos/desktop/wayland/greetd.nix
+++ modules/nixos/desktop/wayland/greetd.nix
@@ -1,10 +1,22 @@
-{ config, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  options.modules.greetd.enable =
-    lib.mkEnableOption "Servicio greetd base";
+  options.modules.greetd = {
+    enable = lib.mkEnableOption "greetd display manager";
+    sessionCommand = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = "Comando de sesión por defecto";
+    };
+  };
 
   config = lib.mkIf config.modules.greetd.enable {
-    services.greetd.enable = true;
+    services.greetd = {
+      enable = true;
+      settings.default_session = {
+        command = config.modules.greetd.sessionCommand;
+        user = "Sayokeoda"; # cámbialo
+      };
+    };
   };
 }
